bc5254658c133025cb8cf08530f53c6e6b7776a1
Tunhoclaptrinh <tuannguyentien16@gmail.com>
2025-05-16 09:35:07 +0700

    TraCuuPublic

---
+++ src/pages/TraCuuPublic/index.tsx
@@ -0,0 +1,93 @@
+import MyDatePicker from '@/components/MyDatePicker';
+import { SearchOutlined } from '@ant-design/icons';
+import { Button, Card, Col, Form, Input, message, Row } from 'antd';
+import { useModel } from 'umi';
+import Footer from '../../components/Footer';
+import Header from '../../components/Header/Header';
+import KetQuaVanBang from './KetQua';
+import rules from '@/utils/rules';
+
+const TraCuuVanBangPublic = () => {
+	const [form] = Form.useForm();
+	// const { formSubmiting, traCuuPhuLucVanBanModel } = useModel('vbcc.phulucvanbang');
+
+	// const onFinish = async (values: any) => {
+	// 	const filledFields = Object.values(values).filter((value) => value).length;
+	// 	if (filledFields < 2) {
+	// 		message.error('Vui lòng nhập ít nhất 2 trường');
+	// 		return;
+	// 	}
+
+	// try {
+	// 	await traCuuPhuLucVanBanModel(values);
+	// } catch (error) {
+	// 	message.error('Có lỗi xảy ra khi tra cứu, vui lòng thử lại sau');
+	// }
+	// };
+
+	return (
+		<>
+			<Header subTitle='Cổng xác thực thông tin văn bằng' />
+			<div
+				style={{
+					// eslint-disable-next-line quotes
+					backgroundImage: `url('/bg-vbcc.png')`,
+					backgroundSize: 'cover',
+					backgroundPosition: 'center',
+					backgroundRepeat: 'no-repeat',
+				}}
+			>
+				<div style={{ maxWidth: 1200, margin: 'auto', paddingTop: 30, paddingBottom: 30 }}>
+					<Card title='Thông tin tra cứu'>
+						<Form
+							// onFinish={onFinish}
+							layout={'vertical'}
+							// form={form}
+						>
+							<Row gutter={[12, 12]}>
+								<Col span={24} md={12}>
+									<Form.Item name='hoTen' label='Họ tên'>
+										<Input placeholder='Nhập họ tên' />
+									</Form.Item>
+								</Col>
+								<Col span={24} md={12}>
+									<Form.Item name='ngaySinh' label='Ngày sinh'>
+										<MyDatePicker placeholder='Chọn ngày sinh' format='DD/MM/YYYY' />
+									</Form.Item>
+								</Col>
+								<Col span={24} md={12}>
+									<Form.Item name='soCCCD' label='Số CMND/CCCD'>
+										<Input placeholder='Nhập số CMND/CCCD' />
+									</Form.Item>
+								</Col>
+								<Col span={24} md={12}>
+									<Form.Item name='soBaoDanh' label='Số báo danh'>
+										<Input placeholder='Nhập số báo danh' />
+									</Form.Item>
+								</Col>
+
+								<Col span={24}>
+									<Button
+										icon={<SearchOutlined />}
+										type='primary'
+										htmlType='submit'
+										// loading={formSubmiting}
+									>
+										Tra cứu
+									</Button>
+								</Col>
+							</Row>
+						</Form>
+					</Card>
+
+					<div style={{ marginTop: 12 }}>
+						<KetQuaVanBang />
+					</div>
+				</div>
+			</div>
+			<Footer />
+		</>
+	);
+};
+
+export default TraCuuVanBangPublic;
