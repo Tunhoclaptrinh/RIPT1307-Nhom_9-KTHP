8bd05d4cc67e2a7fe16ccc578748fe9beec14633
Tunhoclaptrinh <tuannguyentien16@gmail.com>
2025-05-25 02:25:42 +0700

    fix menu

--- config/routes.ts
+++ config/routes.ts
@@ -35,25 +35,75 @@ export default [
 		icon: 'InfoCircleOutlined',
 	},
 	{
-		path: '/trang-chu',
-		name: 'Trang chủ',
-		component: './TrangChuPublic',
-		icon: 'HomeOutlined',
-		layout: false,
+		path: '/public',
+		name: 'Trang thông tin',
+		icon: 'GlobalOutlined',
+		routes: [
+			{
+				path: '/public/trang-chu',
+				name: 'Trang chủ',
+				component: './TrangChuPublic',
+				icon: 'HomeOutlined',
+				layout: false,
+			},
+			{
+				path: '/public/tra-cuu-public',
+				name: 'Tra cứu tuyển sinh',
+				component: './TraCuuPublic',
+				icon: 'SearchOutlined',
+				layout: false,
+			},
+			{
+				path: '/public/dash-board',
+				name: 'Trang cá nhân', // Tramg cá nhân thí sinh
+				component: './ThiSinh/DashBoard',
+				icon: 'UserOutlined',
+				layout: false,
+			},
+		],
 	},
 	{
-		path: '/tra-cuu-public',
-		name: 'Tra cứu tuyển sinh',
-		component: './TraCuuPublic',
-		icon: 'SearchOutlined',
-		layout: false,
-	},
-	{
-		path: '/dash-board',
-		name: 'Trang cá nhân', // Tramg cá nhân thí sinh
-		component: './ThiSinh/DashBoard',
-		icon: 'UserOutlined',
-		layout: false,
+		path: '/public-manage',
+		name: 'Quản lý trang',
+		icon: 'GlobalOutlined',
+		routes: [
+			{
+				path: '/public-manage/thong-bao-tuyen-sinh',
+				name: 'Thông báo tuyển sinh',
+				component: './ThongBaoTS',
+				icon: 'NotificationOutlined',
+			},
+			{
+				path: '/public-manage/huong-dan-ho-so',
+				name: 'Hướng dẫn hồ sơ',
+				component: './HuongDanHS',
+				icon: 'FileTextOutlined',
+			},
+			{
+				path: '/public-manage/hoi-dap',
+				name: 'Hỏi đáp',
+				component: './FAQ',
+				icon: 'QuestionCircleOutlined',
+			},
+			{
+				path: '/public-manage/lich-trinh-tuyen-sinh',
+				name: 'Lịch trình tuyển sinh',
+				component: './LichTrinhTS',
+				icon: 'CalendarOutlined',
+			},
+			{
+				path: '/public-manage/thong-ke-tuyen-sinh',
+				name: 'Thống kê tuyển sinh',
+				component: './ThongKeTS',
+				icon: 'BarChartOutlined',
+			},
+			{
+				path: '/public-manage/tin-tuc',
+				name: 'Tin tức',
+				component: './TinTuc',
+				icon: 'FileSearchOutlined',
+			},
+		],
 	},
 	{
 		path: '/users',
@@ -78,42 +128,6 @@ export default [
 		name: 'Tổ hợp môn học',
 		component: './ToHop',
 		icon: 'ReadOutlined',
-	},
-	{
-		path: '/thong-bao-tuyen-sinh',
-		name: 'Thông báo tuyển sinh',
-		component: './ThongBaoTS',
-		icon: 'NotificationOutlined',
-	},
-	{
-		path: '/huong-dan-ho-so',
-		name: 'Hướng dẫn hồ sơ',
-		component: './HuongDanHS',
-		icon: 'FileTextOutlined',
-	},
-	{
-		path: '/hoi-dap',
-		name: 'Hỏi đáp',
-		component: './FAQ',
-		icon: 'QuestionCircleOutlined',
-	},
-	{
-		path: '/lich-trinh-tuyen-sinh',
-		name: 'Lịch trình tuyển sinh',
-		component: './LichTrinhTS',
-		icon: 'CalendarOutlined',
-	},
-	{
-		path: '/thong-ke-tuyen-sinh',
-		name: 'Thống kê tuyển sinh',
-		component: './ThongKeTS',
-		icon: 'BarChartOutlined',
-	},
-	{
-		path: '/tin-tuc',
-		name: 'Tin tức',
-		component: './TinTuc',
-		icon: 'FileSearchOutlined',
 	},
 
 	// {
