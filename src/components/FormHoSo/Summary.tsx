6f29c46991dfb03516b741aaf71b3b6ec85a008a
Tunhoclaptrinh <tuannguyentien16@gmail.com>
2025-06-05 19:42:00 +0700

    init Summary

--- src/components/FormHoSo/Summary.tsx
+++ src/components/FormHoSo/Summary.tsx
@@ -1,81 +1,190 @@
 import React from 'react';
-import { Card, Descriptions } from 'antd';
+import { Card, Descriptions, Divider } from 'antd';
 import moment from 'moment';
 import PhuongThucXTSelect from '@/pages/PhuongThucXT/components/Select';
 
-const SummaryForm = ({ userId, formData, showHocBa }) => {
+interface SummaryFormProps {
+	userId: string;
+	formData: any;
+	showHocBa: boolean;
+	apiData: any;
+}
+
+const SummaryForm: React.FC<SummaryFormProps> = ({ userId, formData, showHocBa, apiData }) => {
+	// Extract relevant data from formData and apiData
+	const personalInfo = formData.personalInfo || {};
+	const educationGrades = formData.educationGrades || {};
+	const hocBa = formData.hocBa || {};
+	const wishes = formData.wishes || [];
+	const hoSo = apiData.hoSo || {};
+	const thongTinBoSung = hoSo.thongTinBoSung || {};
+	const thongTinLienHe = hoSo.thongTinLienHe || {};
+	const thongTinHocTap = educationGrades || apiData.thongTinHocTap || {};
+	const thongTinTHPT = thongTinHocTap.thongTinTHPT || {};
+	const diemTHPT = thongTinHocTap.diemTHPT || [];
+	const diemDGTD = thongTinHocTap.diemDGTD || {};
+	const diemDGNL = thongTinHocTap.diemDGNL || {};
+	const giaiHSG = thongTinHocTap.giaiHSG || {};
+	const chungChi = thongTinHocTap.chungChi || [];
+	const nguyenVong = wishes.length > 0 ? wishes : apiData.thongTinNguyenVong || [];
+
 	return (
 		<div>
-			<Card title='Xem lại thông tin hồ sơ' style={{ marginBottom: 16 }}>
+			{/* Personal Information */}
+			<Card title='Thông tin cá nhân' style={{ marginBottom: 16 }}>
 				<Descriptions column={2} bordered>
 					<Descriptions.Item label='Họ và tên'>
-						{formData.ho} {formData.ten}
+						{personalInfo.ho || ''} {personalInfo.ten || ''}
 					</Descriptions.Item>
 					<Descriptions.Item label='Ngày sinh'>
-						{formData.ngaySinh ? moment(formData.ngaySinh).format('DD/MM/YYYY') : ''}
+						{personalInfo.ngaySinh ? moment(personalInfo.ngaySinh).format('DD/MM/YYYY') : ''}
 					</Descriptions.Item>
-					<Descriptions.Item label='Email'>{formData.email}</Descriptions.Item>
-					<Descriptions.Item label='Số điện thoại'>{formData.soDT}</Descriptions.Item>
-					<Descriptions.Item label='Số CCCD'>{formData.soCCCD}</Descriptions.Item>
-					<Descriptions.Item label='Giới tính'>{formData.gioiTinh}</Descriptions.Item>
+					<Descriptions.Item label='Email'>{personalInfo.email || ''}</Descriptions.Item>
+					<Descriptions.Item label='Số điện thoại'>{personalInfo.soDT || ''}</Descriptions.Item>
+					<Descriptions.Item label='Số CCCD'>{personalInfo.soCCCD || ''}</Descriptions.Item>
+					<Descriptions.Item label='Giới tính'>{personalInfo.gioiTinh || ''}</Descriptions.Item>
+					<Descriptions.Item label='Ngày cấp CCCD'>
+						{personalInfo.ngayCap ? moment(personalInfo.ngayCap).format('DD/MM/YYYY') : ''}
+					</Descriptions.Item>
+					<Descriptions.Item label='Nơi cấp CCCD'>{personalInfo.noiCap || ''}</Descriptions.Item>
+					<Descriptions.Item label='Hộ khẩu thường trú'>
+						{personalInfo.hoKhauThuongTru?.diaChi || ''}, {personalInfo.hoKhauThuongTru?.xaPhuong || ''},{' '}
+						{personalInfo.hoKhauThuongTru?.quanHuyen || ''}, {personalInfo.hoKhauThuongTru?.tinh_ThanhPho || ''}
+					</Descriptions.Item>
+					<Descriptions.Item label='Thông tin liên hệ'>
+						{thongTinLienHe.diaChi?.diaChiCuThe || ''}, {thongTinLienHe.diaChi?.xaPhuong || ''},{' '}
+						{thongTinLienHe.diaChi?.quanHuyen || ''}, {thongTinLienHe.diaChi?.tinh_ThanhPho || ''}
+					</Descriptions.Item>
+					<Descriptions.Item label='Dân tộc'>{thongTinBoSung.danToc || 'Không'}</Descriptions.Item>
+					<Descriptions.Item label='Quốc tịch'>{thongTinBoSung.quocTich || 'Không'}</Descriptions.Item>
+					<Descriptions.Item label='Tôn giáo'>{thongTinBoSung.tonGiao || 'Không'}</Descriptions.Item>
+					<Descriptions.Item label='Nơi sinh'>
+						{thongTinBoSung.noiSinh?.trongNuoc
+							? `${thongTinBoSung.noiSinh?.tinh_ThanhPho || ''}`
+							: thongTinBoSung.noiSinh?.quocGia || 'Không'}
+					</Descriptions.Item>
 				</Descriptions>
 			</Card>
 
-			{formData.thongTinTHPT && (
+			{/* High School Information */}
+			{thongTinTHPT && (
 				<Card title='Thông tin trường THPT' style={{ marginBottom: 16 }}>
-					<Descriptions column={2}>
-						<Descriptions.Item label='Mã trường'>{formData.thongTinTHPT.maTruong}</Descriptions.Item>
-						<Descriptions.Item label='Tỉnh/Thành phố'>{formData.thongTinTHPT.tinh_ThanhPho}</Descriptions.Item>
-						<Descriptions.Item label='Khu vực ưu tiên'>{formData.thongTinTHPT.khuVucUT}</Descriptions.Item>
-						<Descriptions.Item label='Đối tượng ưu tiên'>
-							{formData.thongTinTHPT.doiTuongUT || 'Không'}
-						</Descriptions.Item>
+					<Descriptions column={2} bordered>
+						<Descriptions.Item label='Tên trường'>{thongTinTHPT.ten || ''}</Descriptions.Item>
+						<Descriptions.Item label='Mã trường'>{thongTinTHPT.maTruong || ''}</Descriptions.Item>
+						<Descriptions.Item label='Tỉnh/Thành phố'>{thongTinTHPT.tinh_ThanhPho || ''}</Descriptions.Item>
+						<Descriptions.Item label='Quận/Huyện'>{thongTinTHPT.quanHuyen || ''}</Descriptions.Item>
+						<Descriptions.Item label='Xã/Phường'>{thongTinTHPT.xaPhuong || ''}</Descriptions.Item>
+						<Descriptions.Item label='Địa chỉ'>{thongTinTHPT.diaChi || ''}</Descriptions.Item>
+						<Descriptions.Item label='Khu vực ưu tiên'>{thongTinTHPT.khuVucUT || 'Không'}</Descriptions.Item>
+						<Descriptions.Item label='Đối tượng ưu tiên'>{thongTinTHPT.doiTuongUT || 'Không'}</Descriptions.Item>
+						<Descriptions.Item label='Đã tốt nghiệp'>{thongTinTHPT.daTotNghiep ? 'Có' : 'Không'}</Descriptions.Item>
+						<Descriptions.Item label='Năm tốt nghiệp'>{thongTinTHPT.namTotNghiep || ''}</Descriptions.Item>
 					</Descriptions>
 				</Card>
 			)}
 
-			{formData.diemTHPT && formData.diemTHPT.length > 0 && (
+			{/* THPT Scores */}
+			{diemTHPT.length > 0 && (
 				<Card title='Điểm THPT đã nhập' style={{ marginBottom: 16 }}>
-					{formData.diemTHPT.map((diem, index) => (
-						<div key={index} style={{ marginBottom: 8 }}>
-							<strong>{diem.mon}</strong>: {diem.diem} điểm
-						</div>
-					))}
+					<Descriptions column={1} bordered>
+						{diemTHPT.map((diem: any, index: number) => (
+							<Descriptions.Item key={index} label={diem.mon}>
+								{diem.diem} điểm
+							</Descriptions.Item>
+						))}
+					</Descriptions>
 				</Card>
 			)}
 
-			{showHocBa && formData.diemMonHoc && formData.diemMonHoc.length > 0 && (
+			{/* Academic Transcript (Học bạ) */}
+			{showHocBa && hocBa.diemMonHoc && hocBa.diemMonHoc.length > 0 && (
 				<Card title='Điểm học bạ đã nhập' style={{ marginBottom: 16 }}>
-					{formData.diemMonHoc.map((diem, index) => (
-						<div key={index} style={{ marginBottom: 8 }}>
-							<strong>{diem.mon}</strong> - {diem.hocKy}: {diem.diemTongKet} điểm
-						</div>
-					))}
+					<Descriptions column={1} bordered>
+						{hocBa.diemMonHoc.map((diem: any, index: number) => (
+							<Descriptions.Item key={index} label={`${diem.mon} - ${diem.hocKy}`}>
+								{diem.diemTongKet} điểm
+							</Descriptions.Item>
+						))}
+						<Descriptions.Item label='Loại hạnh kiểm'>{hocBa.loaiHanhKiem || 'Không'}</Descriptions.Item>
+						<Descriptions.Item label='Minh chứng'>{hocBa.minhChung || 'Không'}</Descriptions.Item>
+					</Descriptions>
 				</Card>
 			)}
 
-			{formData.chungChi && formData.chungChi.length > 0 && (
+			{/* DGTD Scores */}
+			{diemDGTD.mon && diemDGTD.mon.length > 0 && (
+				<Card title='Điểm đánh giá tư duy (DGTD)' style={{ marginBottom: 16 }}>
+					<Descriptions column={1} bordered>
+						{diemDGTD.mon.map((mon: any, index: number) => (
+							<Descriptions.Item key={index} label={mon.ten}>
+								{mon.diem} điểm
+							</Descriptions.Item>
+						))}
+						<Descriptions.Item label='Tổng điểm'>{diemDGTD.tongDiem || 0}</Descriptions.Item>
+						<Descriptions.Item label='Minh chứng'>{diemDGTD.minhChung || 'Không'}</Descriptions.Item>
+					</Descriptions>
+				</Card>
+			)}
+
+			{/* DGNL Scores */}
+			{diemDGNL.mon && diemDGNL.mon.length > 0 && (
+				<Card title='Điểm đánh giá năng lực (DGNL)' style={{ marginBottom: 16 }}>
+					<Descriptions column={1} bordered>
+						{diemDGNL.mon.map((mon: any, index: number) => (
+							<Descriptions.Item key={index} label={mon.ten}>
+								{mon.diem} điểm
+							</Descriptions.Item>
+						))}
+						<Descriptions.Item label='Tổng điểm'>{diemDGNL.tongDiem || 0}</Descriptions.Item>
+						<Descriptions.Item label='Minh chứng'>{diemDGNL.minhChung || 'Không'}</Descriptions.Item>
+					</Descriptions>
+				</Card>
+			)}
+
+			{/* Academic Awards (Giải HSG) */}
+			{giaiHSG.giaiHsgCap && (
+				<Card title='Giải học sinh giỏi' style={{ marginBottom: 16 }}>
+					<Descriptions column={1} bordered>
+						<Descriptions.Item label='Cấp giải'>{giaiHSG.giaiHsgCap || 'Không'}</Descriptions.Item>
+						<Descriptions.Item label='Môn'>{giaiHSG.mon || 'Không'}</Descriptions.Item>
+						<Descriptions.Item label='Loại giải'>{giaiHSG.loaiGiai || 'Không'}</Descriptions.Item>
+						<Descriptions.Item label='Năm'>{giaiHSG.nam || 'Không'}</Descriptions.Item>
+						<Descriptions.Item label='Nơi cấp'>{giaiHSG.noiCap || 'Không'}</Descriptions.Item>
+						<Descriptions.Item label='Minh chứng'>{giaiHSG.minhChung || 'Không'}</Descriptions.Item>
+					</Descriptions>
+				</Card>
+			)}
+
+			{/* Certificates (Chứng chỉ) */}
+			{chungChi.length > 0 && (
 				<Card title='Chứng chỉ đã nhập' style={{ marginBottom: 16 }}>
-					{formData.chungChi.map((cc, index) => (
-						<div key={index} style={{ marginBottom: 8 }}>
-							<strong>{cc.loaiCC}</strong>: {cc.ketQua}
-							{cc.minhChung && ` - Minh chứng: ${cc.minhChung}`}
-						</div>
-					))}
+					<Descriptions column={1} bordered>
+						{chungChi.map((cc: any, index: number) => (
+							<Descriptions.Item key={index} label={cc.loaiCC}>
+								{cc.ketQua} {cc.minhChung && `(Minh chứng: ${cc.minhChung})`}
+							</Descriptions.Item>
+						))}
+					</Descriptions>
 				</Card>
 			)}
 
-			{formData.nguyenVong && formData.nguyenVong.length > 0 && (
+			{/* Admission Preferences (Nguyện vọng) */}
+			{nguyenVong.length > 0 && (
 				<Card title='Nguyện vọng đã chọn' style={{ marginBottom: 16 }}>
-					{formData.nguyenVong.map((nv, index) => (
+					{nguyenVong.map((nv: any, index: number) => (
 						<Card key={index} type='inner' title={`Nguyện vọng ${index + 1}`} style={{ marginBottom: 8 }}>
-							<Descriptions column={1} size='small'>
-								<Descriptions.Item label='Ngành đào tạo'>{nv.ten}</Descriptions.Item>
-								<Descriptions.Item label='Phương thức xét tuyển'>{nv.phuongThucId}</Descriptions.Item>
-								<Descriptions.Item label='Điểm chưa ưu tiên'>{nv.diemChuaUT}</Descriptions.Item>
-								<Descriptions.Item label='Điểm có ưu tiên'>{nv.diemCoUT}</Descriptions.Item>
-								<Descriptions.Item label='Tổng điểm'>{nv.tongDiem}</Descriptions.Item>
-								<Descriptions.Item label='Cơ sở đào tạo'>{nv.coSoDaoTao}</Descriptions.Item>
+							<Descriptions column={1} size='small' bordered>
+								<Descriptions.Item label='Ngành đào tạo'>{nv.ten || ''}</Descriptions.Item>
+								<Descriptions.Item label='Phương thức xét tuyển'>
+									{apiData.phuongThucXetTuyen.find((pt: any) => pt.id === nv.phuongThucId)?.ten || nv.phuongThucId}
+								</Descriptions.Item>
+								<Descriptions.Item label='Điểm chưa ưu tiên'>{nv.diemChuaUT || 0}</Descriptions.Item>
+								<Descriptions.Item label='Điểm có ưu tiên'>{nv.diemCoUT || 0}</Descriptions.Item>
+								<Descriptions.Item label='Điểm ưu tiên đối tượng'>{nv.diemDoiTuongUT || 0}</Descriptions.Item>
+								<Descriptions.Item label='Điểm ưu tiên khu vực'>{nv.diemKhuVucUT || 0}</Descriptions.Item>
+								<Descriptions.Item label='Tổng điểm'>{nv.tongDiem || 0}</Descriptions.Item>
+								<Descriptions.Item label='Cơ sở đào tạo'>{nv.coSoDaoTao || 'Không'}</Descriptions.Item>
 								{nv.ghiChu && <Descriptions.Item label='Ghi chú'>{nv.ghiChu}</Descriptions.Item>}
 							</Descriptions>
 						</Card>
@@ -83,6 +192,38 @@ const SummaryForm = ({ userId, formData, showHocBa }) => {
 				</Card>
 			)}
 
+			{/* Application Status */}
+			{hoSo.tinhTrang && (
+				<Card title='Tình trạng hồ sơ' style={{ marginBottom: 16 }}>
+					<Descriptions column={1} bordered>
+						<Descriptions.Item label='Tình trạng'>{hoSo.tinhTrang || 'Chưa nộp'}</Descriptions.Item>
+						{hoSo.ketQua && (
+							<>
+								<Descriptions.Item label='Kết quả'>
+									{hoSo.ketQua.success || hoSo.ketQua.succes ? 'Đạt' : 'Không đạt'}
+								</Descriptions.Item>
+								{hoSo.ketQua.nguyenVong && (
+									<Descriptions.Item label='Nguyện vọng trúng tuyển'>
+										{nguyenVong.find((nv: any) => nv.id === hoSo.ketQua.nguyenVong)?.ten || hoSo.ketQua.nguyenVong}
+									</Descriptions.Item>
+								)}
+								{hoSo.ketQua.nguyenVongDo && (
+									<Descriptions.Item label='Nguyện vọng trúng tuyển'>
+										{nguyenVong.find((nv: any) => nv.id === hoSo.ketQua.nguyenVongDo)?.ten || hoSo.ketQua.nguyenVongDo}
+									</Descriptions.Item>
+								)}
+								<Descriptions.Item label='Điểm trúng tuyển'>{hoSo.ketQua.diem || 'Không có'}</Descriptions.Item>
+								<Descriptions.Item label='Phương thức xét tuyển'>
+									{apiData.phuongThucXetTuyen.find((pt: any) => pt.id === hoSo.ketQua.phuongThucId)?.ten ||
+										hoSo.ketQua.phuongThucId}
+								</Descriptions.Item>
+							</>
+						)}
+					</Descriptions>
+				</Card>
+			)}
+
+			{/* Confirmation */}
 			<Card title='Xác nhận nộp hồ sơ'>
 				<p>
 					Tôi xác nhận rằng tất cả thông tin đã cung cấp là chính xác và cam kết chịu trách nhiệm về tính xác thực của
